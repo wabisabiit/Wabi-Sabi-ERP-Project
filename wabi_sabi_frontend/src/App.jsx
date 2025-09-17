aed13e6783ca3d7428322e933f860973b674e955
wabisabiit <wabisabi.it268@gmail.com>
2025-09-17 11:26:35 +0000

    17-09-2025 Ranjit order list  Done Commit

--- wabi_sabi_frontend/src/App.jsx
+++ wabi_sabi_frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Sidebar from "./components/Sidebar";
 import MultiplePay from "./components/MultiplePay";
 import CreditNotePage from "./components/CreditNotePage";
 import SalesRegisterPage from "./components/SalesRegisterPage";
-import OrderList from "./components/OrderList";   
+import OrderList from "./components/OrderListPage";
 import "./App.css";
 
 function POSLayout() {
